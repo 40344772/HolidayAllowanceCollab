b2db0f12b23befc36f1226c16f6d3143da862391
40345238 <40345238@live.napier.ac.uk>
2021-03-11 14:18:28 +0000

    updated everything

---
+++ HolidayAllowance-master/haeUI.py
@@ -0,0 +1,15 @@
+﻿#HAEUI (Holiday Allowance Engine User Interface)
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__) 
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug = True) #currently runs on debug mode to catch errors
