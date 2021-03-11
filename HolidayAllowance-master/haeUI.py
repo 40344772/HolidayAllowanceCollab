ec633bf5cd9a991cd8294e80e40bbae5ad612b9d
Robert Ewing <40344772@live.napier.ac.uk>
2021-03-11 19:18:06 +0000

    renaming files

--- HolidayAllowance-master/haeUI.py
+++ HolidayAllowance-master/haeUI.py
@@ -9,7 +9,7 @@ def login():
 
 @app.route('/home')
 def index():
-    return render_template('index.html')
+    return render_template('request.html')
 
 if __name__ == "__main__":
     app.run(debug = True) #currently runs on debug mode to catch errors
